4c9e018d047a0457970a88469b6971b8b71867f9
ManjunathBE <31193651+ManjunathBE@users.noreply.github.com>
2020-07-06 13:54:34 +0530

    Broken our homepage into smaller components

---
+++ src/pages/homepage/homepage.component.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./HomePage.styles.scss";
+import Directory from "../../components/directory/directory.component";
+
+export default function homepage() {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+}
